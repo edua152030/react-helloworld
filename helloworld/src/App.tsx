df78e5d484c9f3cdae00fef44b3abce1ddd7e80f
edua152030 <eduardo.vaz.faria@gmail.com>
2022-10-24 12:49:57 -0300

    aula pratica

--- helloworld/src/App.tsx
+++ helloworld/src/App.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
 import './App.css';
 
-import CadastroCliente from './componentes/CadastroCliente'
-
-function App() {
-
-  const empresa: string = "Cursos REACT"
-
-  const rsCliente = {
-    nome: 'Zanatta',
-    endereco: 'Av. 21 de Abril 504'
-  }
+export default function App() {
 
 
-  return (
-    <div className="App">
+  return(
+    <>
+      <h1>teste</h1>
+    </>
+  )
 
-      <h1>{empresa}</h1>
-      <CadastroCliente />
-      {/* comentarios */}
-
-    </div>
-  );
-}
-
-export default App;
+};
